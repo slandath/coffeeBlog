4b22e4475f7bcad9b4f0d9bb31c06a81d4607a22
slandath <jesus_slanda@yahoo.com>
2025-01-18 22:13:44 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Coffee Blog
+
+A simple blog using Eleventy with Liquid templates and Bulma CSS.
+
+## Built With
+
+[![Eleventy](https://img.shields.io/badge/Eleventy-black?logo=eleventy)](#)  
+[![Bulma](https://img.shields.io/badge/-Bulma-00D1B2?style=flat&logo=bulma&logoColor=white)](#)  
+
+## Docs
+
+[Eleventy Docs](https://www.11ty.dev/docs/)  
+[Liquid Docs](https://shopify.github.io/liquid/)  
+[Bulma Docs](https://bulma.io/documentation/)
---
+++ eleventy.config.js
@@ -0,0 +1,30 @@
+export default function(eleventyConfig) {
+  eleventyConfig.addCollection("brands", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("./brands/*.liquid")
+  });
+  eleventyConfig.addCollection("equipment", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("./equipment/*.liquid")
+  });
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("./blog/*.liquid").sort((a, b) => b.date - a.date);
+  });
+  eleventyConfig.addCollection("resources", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("./resources/*.liquid")
+  });
+  eleventyConfig.addCollection("shops", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("./shops/*.liquid")
+  });
+  eleventyConfig.addPassthroughCopy("bundle.css");  
+  eleventyConfig.setLiquidOptions({
+      dynamicPartials: false,
+      jsTruthy: true,
+    });
+    return {
+      dir: {
+        input: ".", // Source files root directory
+        includes: "_includes", // Directory for layouts, partials, and includes
+        layouts: "_includes/layouts", // Subdirectory for layouts specifically
+        output: "_site", // Output directory for the built site
+      },
+    };
+  };
