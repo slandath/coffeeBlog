43eb3c63f2a703efa139ec21878e563af495cbad
Tom Slanda <jesus_slanda@yahoo.com>
2026-01-14 15:58:13 -0500

    formatted files (#18)
    Signed-off-by: Tom Slanda <jesus_slanda@yahoo.com>

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ A simple blog using Eleventy with Liquid templates.
 
 ## Built With
 
-[![Eleventy](https://img.shields.io/badge/Eleventy-black?logo=eleventy)](#)  
+[![Eleventy](https://img.shields.io/badge/Eleventy-black?logo=eleventy)](#)
 
 ## Docs
 
 [Eleventy Docs](https://www.11ty.dev/docs/)  
-[Liquid Docs](https://shopify.github.io/liquid/)  
+[Liquid Docs](https://shopify.github.io/liquid/)
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,35 +1,37 @@
-export default function(eleventyConfig) {
-  eleventyConfig.addCollection("brands", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("./brands/*.liquid")
+export default function (eleventyConfig) {
+  eleventyConfig.addCollection("brands", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("./brands/*.liquid");
   });
-  eleventyConfig.addCollection("equipment", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("./equipment/*.liquid")
+  eleventyConfig.addCollection("equipment", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("./equipment/*.liquid");
   });
-  eleventyConfig.addCollection("posts", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("./blog/*.liquid").sort((a, b) => b.date - a.date);
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi
+      .getFilteredByGlob("./blog/*.liquid")
+      .sort((a, b) => b.date - a.date);
   });
-  eleventyConfig.addCollection("recipes", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("./recipes/*.liquid")
+  eleventyConfig.addCollection("recipes", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("./recipes/*.liquid");
   });
-  eleventyConfig.addCollection("resources", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("./resources/*.liquid")
+  eleventyConfig.addCollection("resources", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("./resources/*.liquid");
   });
-  eleventyConfig.addCollection("shops", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("./shops/*.liquid")
+  eleventyConfig.addCollection("shops", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("./shops/*.liquid");
   });
   eleventyConfig.addPassthroughCopy("main.css");
   eleventyConfig.addPassthroughCopy("script.js");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.setLiquidOptions({
-      dynamicPartials: false,
-      jsTruthy: true,
-    });
-    return {
-      dir: {
-        input: ".", // Source files root directory
-        includes: "_includes", // Directory for layouts, partials, and includes
-        layouts: "_includes/layouts", // Subdirectory for layouts specifically
-        output: "_site", // Output directory for the built site
-      },
-    };
+    dynamicPartials: false,
+    jsTruthy: true,
+  });
+  return {
+    dir: {
+      input: ".", // Source files root directory
+      includes: "_includes", // Directory for layouts, partials, and includes
+      layouts: "_includes/layouts", // Subdirectory for layouts specifically
+      output: "_site", // Output directory for the built site
+    },
   };
+}
